4df6cd466f601060d153f5953bd8342801d0d537
szuter <piotrstrzelec1989@gmail.com>
2025-02-12 14:41:27 +0100

    Add feed management functionality with listing

---
+++ handler_feed.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"gator/internal/database"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func handlerAddFeed(s *state, cmd command) error {
+	if len(cmd.args) < 2 {
+		return fmt.Errorf("add-feed requires two arguments")
+	}
+	name := cmd.args[0]
+	url := cmd.args[1]
+	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return fmt.Errorf("couldn't find user: %w", err)
+	}
+	rssFeed, err := s.db.AddFeed(context.Background(), database.AddFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+		Url:       url,
+		UserID:    uuid.NullUUID{UUID: user.ID, Valid: true},
+	})
+	if err != nil {
+		return fmt.Errorf("failed to add feed: %w", err)
+	}
+	fmt.Printf("Feed added:\n ID: %v\n Name: %s\n URL: %s\n UserID: %v\n CreatedAt: %s\n UpdatedAt: %s\n", rssFeed.ID, rssFeed.Name, rssFeed.Url, rssFeed.UserID.UUID, rssFeed.CreatedAt, rssFeed.UpdatedAt)
+	return nil
+}
+
+func handlerListFeeds(s *state, _ command) error {
+	rssFeeds, err := s.db.GetFeeds(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't get feeds: %w", err)
+	}
+	for _, rssFeed := range rssFeeds {
+
+		user, err := s.db.GetUserByID(context.Background(), rssFeed.UserID.UUID)
+		if err != nil {
+			return fmt.Errorf("couldn't get user: %w", err)
+		}
+		printFeed(rssFeed, user.Name)
+	}
+	return nil
+}
+
+func printFeed(feed database.Feed, userName string) {
+	fmt.Printf("%s | %s | %s\n", userName, feed.Name, feed.Url)
+}
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -16,3 +16,6 @@ DELETE FROM users;
 
 -- name: GetUsers :many
 SELECT * FROM users;
+
+-- name: GetUserByID :one
+SELECT * FROM users WHERE id = $1;
